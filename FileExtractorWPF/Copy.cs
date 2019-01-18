5932713fa49a69a8e823c71bdbeb8532402ef14b
Damián Cabello <jdamiancabello@gmail.com>
2019-01-18 14:30:46 +0100

    Some bugs fixed

---
+++ FileExtractorWPF/Copy.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+
+namespace CodeExtractor
+{
+    static class Copy
+    {
+
+        /// <summary>
+        /// Copy expecific files filtering by extension
+        /// </summary>
+        /// <param name="sourcePath">Original full directory patch</param>
+        /// <param name="destPath">Destination full directory patch</param>
+        /// <param name="cs">search all *.cs extensions in the directory</param>
+        /// <param name="exe">search all *.exe extensions in the directory</param>
+        /// <param name="dll">search all *.dll extensions in the directory</param>
+        /// <param name="compress">Determines if the folder is compressed</param>
+        /// <param name="deleteAfterCompress">Determines if the compressed directory should be deleted after zip</param>
+        public static void ExpecificFiles(string sourcePath, string destPath, bool cs, bool exe, bool dll, bool compress, bool deleteAfterCompress, bool showCreatedFileInExplorer)
+        {
+            if (sourcePath == "" || !Directory.Exists(sourcePath))
+                return;
+
+            string[] directorys = Directory.GetDirectories(sourcePath);
+            string[] filesToCopy = null;
+            string[] extensionsToCopy = CreateExtensionList(cs, exe, dll);
+
+            for (int i = 0; i < directorys.Length; i++)
+            {
+                filesToCopy = SearchFilesToCopy(directorys[i], extensionsToCopy);
+                if (!Directory.Exists(DirectoryEndPatch(directorys[i], destPath )))
+                    Directory.CreateDirectory(DirectoryEndPatch(directorys[i], destPath));
+
+                for (int j = 0; j < filesToCopy.Length; j++)
+                    File.Copy(filesToCopy[j], FileEndPatch(directorys[i], destPath, Path.GetFileName(filesToCopy[j])), true);
+            }
+            if(compress)
+                Compress(destPath, Path.GetFileName(destPath), deleteAfterCompress);
+            if (showCreatedFileInExplorer)
+                OpenPatchWindowsExplorer(destPath);
+        }
+
+        /// <summary>
+        /// Method that makes a zip from entire folder
+        /// </summary>
+        /// <param name="path">Patch to make the zip</param>
+        /// <param name="fileName">Zip name</param>
+        /// <param name="deleteDirectoryAfterZip">Determines if the compressed directory should be deleted after zip</param>
+        private static void Compress (string path, string fileName, bool deleteDirectoryAfterZip)
+        {
+            string rutaDirectorioPadre = new DirectoryInfo(path).Parent.FullName + Path.DirectorySeparatorChar;
+            if (File.Exists(rutaDirectorioPadre + fileName + ".zip"))
+                File.Delete(rutaDirectorioPadre + fileName + ".zip");
+
+            ZipFile.CreateFromDirectory(path, rutaDirectorioPadre + fileName + ".zip",CompressionLevel.Optimal,false);
+            if (deleteDirectoryAfterZip)
+                DeleteEntireFolder(path);
+        }
+
+        /// <summary>
+        /// Open the windows explorer to show a file
+        /// </summary>
+        /// <param name="path">the patch to the file</param>
+        private static void OpenPatchWindowsExplorer(string path)
+        {
+            string rutaDirectorioPadre = new DirectoryInfo(path).Parent.FullName + Path.DirectorySeparatorChar;
+            Process.Start(rutaDirectorioPadre);
+        }
+
+        /// <summary>
+        /// Remove recursively a directory
+        /// </summary>
+        /// <param name="pathToDelete">Patch to remove</param>
+        private static void DeleteEntireFolder(string pathToDelete)
+        {
+            Directory.Delete(pathToDelete,true);
+        }
+
+        /// <summary>
+        /// Aux method. Make a end directory patch from source patch and destination patch
+        /// </summary>
+        /// <param name="sourcePath">Original full directory patch</param>
+        /// <param name="destPath">Destination full directory patch</param>
+        /// <returns>The end directory pach</returns>
+        private static string DirectoryEndPatch(string sourcePath, string destPath)
+        {
+            return destPath + Path.DirectorySeparatorChar + Path.GetFileName(sourcePath);
+        }
+
+        /// <summary>
+        /// Aux method. Make a end file patch from source patch and destination patch
+        /// </summary>
+        /// <param name="sourcePath">Original full file patch</param>
+        /// <param name="destPath">Destination full file patch</param>
+        /// <param name="fileName">File name and file extesion</param>
+        /// <returns>The end file patch</returns>
+        private static string FileEndPatch(string sourcePath, string destPath, string fileName)
+        {
+            return destPath + Path.DirectorySeparatorChar + Path.GetFileName(sourcePath) + Path.DirectorySeparatorChar + fileName;
+        }
+
+        /// <summary>
+        /// Search all files in folder by extension and exludes directorys or files
+        /// </summary>
+        /// <param name="folderPath">Full patch to father folder</param>
+        /// <param name="extension">Extension to search in father folder</param>
+        /// <returns></returns>
+        private static string[] SearchFilesToCopy(string folderPath, string[] extension)
+        {
+            List<string> files = new List<string>();
+            string[] excludesPatterns = { Path.DirectorySeparatorChar + "obj" + Path.DirectorySeparatorChar + "Debug" + Path.DirectorySeparatorChar, Path.DirectorySeparatorChar +"Properties" + Path.DirectorySeparatorChar, ".vshost." };
+            for (int i = 0; i < extension.Length; i++)
+            {
+                string[] filePaths = Directory.GetFiles(folderPath, extension[i],SearchOption.AllDirectories); //Search in the folder and subfolders
+                for (int j = 0; j < filePaths.Length; j++)
+                    if (!ExcludedFilesOrFolders(filePaths[j], excludesPatterns))
+                        files.Add(filePaths[j]); 
+            }
+
+            return files.ToArray();
+        }
+        /// <summary>
+        /// Check if these file or folder should be excluded from copy
+        /// </summary>
+        /// <param name="patch">Patch to file</param>
+        /// <param name="excludedPatterns">All excludes patterns</param>
+        /// <returns>A list of patch´s to copy</returns>
+        private static bool ExcludedFilesOrFolders(string patch, string[] excludedPatterns)
+        {
+            for (int i = 0; i < excludedPatterns.Length; i++)
+                if (patch.Contains(excludedPatterns[i]))
+                    return true;
+
+            return false;
+
+
+        }
+
+        /// <summary>
+        /// Aux method for save extensions to search.
+        /// </summary>
+        /// <param name="cs">search all *.cs extensions in the directory</param>
+        /// <param name="exe">search all *.exe extensions in the directory</param>
+        /// <param name="dll">search all *.dll extensions in the directory</param>
+        /// <returns>A list of extensions to search</returns>
+        private static string[] CreateExtensionList(bool cs, bool exe, bool dll)
+        {
+            List<string> auxList = new List<string>();
+
+            if (cs)
+                auxList.Add("*.cs");
+            if (exe)
+                auxList.Add("*.exe");
+            if (dll)
+                auxList.Add("*.dll");
+
+            return auxList.ToArray();
+        }
+    }
+
+
+}
---
+++ FileExtractorWPF/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using CodeExtractor;
+using FolderSelect; //Original author http://www.lyquidity.com/devblog/?p=136 (MIT license)
+//-------------------
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FileExtractorWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRutaInicialClick(object sender, RoutedEventArgs e)
+        {
+
+            var fsd = new FolderSelectDialog();
+            fsd.Title = "Selecciona la carpeta con tus ejercicios";
+            fsd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            btnRutaFinal.IsEnabled = false;
+            this.IsEnabled = false;
+            if (fsd.ShowDialog(IntPtr.Zero))
+            {
+                tbxRutaInicio.Text = fsd.FileName;
+                if(exprArbol.IsExpanded)
+                    ListarDirectorios(trvVisorCarpetas, fsd.FileName);              
+            }
+            btnRutaFinal.IsEnabled = true;
+            this.IsEnabled = true;
+        }
+
+        
+        private void ListarDirectorios(System.Windows.Controls.TreeView treeView, string path)
+        {
+            treeView.Items.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            try
+            {
+                treeView.Items.Add(CrearDirectoriosyNodos(rootDirectoryInfo));
+            }
+            catch
+            {
+                treeView.Items.Add(new TreeViewItem { Header = "ERROR de permisos, no se carga nada aún así busca" });
+            }
+            exprArbol.IsExpanded = true;
+        }
+
+        private TreeViewItem CrearDirectoriosyNodos(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeViewItem { Header = directoryInfo.Name };
+
+
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Items.Add(CrearDirectoriosyNodos(directory));
+
+            foreach (var file in directoryInfo.GetFiles())
+                directoryNode.Items.Add(new TreeViewItem { Header = file.Name });
+
+            return directoryNode;
+
+        }
+
+        private void CbxExpand_Checked(object sender, RoutedEventArgs e)
+        {
+            exprArbol.IsExpanded = true;
+            exprArbol.Visibility = Visibility.Visible;
+            if (Directory.Exists(tbxRutaInicio.Text))
+                ListarDirectorios(trvVisorCarpetas, tbxRutaInicio.Text);
+        }
+
+        private void CbxExpand_Unchecked(object sender, RoutedEventArgs e)
+        {
+            exprArbol.IsExpanded = false;
+            exprArbol.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            Copy.ExpecificFiles(tbxRutaInicio.Text, tbxRutaFinal.Text, cbxCs.IsChecked.Value, cbxExe.IsChecked.Value, cbxDll.IsChecked.Value, cbxComprimir.IsChecked.Value, cbxBorrar.IsChecked.Value, cbxExploradorWindows.IsChecked.Value);
+        }
+
+        private void btnRutaFinalClick(object sender, RoutedEventArgs e)
+        {
+            var fsd = new FolderSelectDialog();
+            fsd.Title = "Selecciona la carpeta para extarer los ficheros";
+            fsd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            btnRutaInicio.IsEnabled = false;
+            this.IsEnabled = false;
+            if (fsd.ShowDialog(IntPtr.Zero))
+            {
+                tbxRutaFinal.Text = fsd.FileName;
+            }
+            btnRutaInicio.IsEnabled = true;
+            this.IsEnabled = true;
+        }
+
+        private void ExpanderCollapsed(object sender, RoutedEventArgs e)
+        {
+            ventanaPrincipal.Width = 372.826;
+        }
+
+        private void ExpanderExpanded(object sender, RoutedEventArgs e)
+        {
+            ventanaPrincipal.Width = 691.161;
+        }
+
+        private void TbxRutaInicio_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Directory.Exists(tbxRutaInicio.Text))
+                if (exprArbol.IsExpanded)
+                    ListarDirectorios(trvVisorCarpetas, tbxRutaInicio.Text);
+                else if (tbxRutaInicio.Text == "" || Directory.Exists(tbxRutaInicio.Text))
+                {
+                    trvVisorCarpetas.Items.Clear();
+                    exprArbol.IsExpanded = false;
+                }
+        }
+
+        private void CbxComprimir_Checked(object sender, RoutedEventArgs e)
+        {
+            cbxBorrar.IsEnabled = cbxComprimir.IsChecked.Value;
+        }
+
+        private void CbxComprimir_Unchecked(object sender, RoutedEventArgs e)
+        {
+            cbxBorrar.IsEnabled = cbxComprimir.IsChecked.Value;
+            cbxBorrar.IsChecked = cbxComprimir.IsChecked.Value;
+        }
+    }
+}
